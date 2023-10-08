2edc675d3d95e5166ecdcfa15570a5e1599e3df3
Hamza EL Aidi <hamza.elaidi@externe.bnpparibas.com>
2023-10-08 20:20:56 +0200

    init project

---
+++ FDJClubs/FDJClubs/FDJClubsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDJClubsApp.swift
+//  FDJClubs
+//
+//  Created by Hamza EL Aidi on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FDJClubsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
